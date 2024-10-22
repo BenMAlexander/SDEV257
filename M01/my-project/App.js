5216e369514470730d8bdfec24abb0cbe1829334
Ben Alexander <126036161+BenMAlexander@users.noreply.github.com>
2024-10-22 14:18:31 -0400

    Update App.js

--- M01/my-project/App.js
+++ M01/my-project/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>My Name is Ben Alexander. I am a Mobile Developer!</Text>
+      <Text style={styles.text}>Hi, my name is Ben Alexander, and I am a mobile developer</Text>
       <StatusBar style="auto" />
     </View>
   );
