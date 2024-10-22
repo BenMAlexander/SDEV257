5b109522146389f946ab1eb265f0c6b3c87929a9
Ben Alexander <126036161+BenMAlexander@users.noreply.github.com>
2024-10-22 13:59:33 -0400

    start

---
+++ M01/my-project/App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>My Name is Ben Alexander. I am a Mobile Developer!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontWeight: "bold", 
+    },
+});
